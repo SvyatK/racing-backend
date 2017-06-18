e2671300056f7b59b25ea67ec2acc4218d2aac3b
SvyatK <Svyat_@ukr.net>
2017-06-18 13:59:45 +0300

    implemented socket connection;

--- services/socketWrapper.js
+++ services/socketWrapper.js
@@ -1,21 +1,71 @@
 
 var io;
 
-var clientCount;
+var clientCount=0;
+
+var clientsReady=0;
+
+var timeout;
+
+var items = [];
+
+var startCoordinates = [{x:3104, y:2287, rot:-1.49},{x:3175, y:2292, rot:-1.49}]
 
 var setSocket = function (data) {
     io = data;
     io.on('connection', function(client){
-        clientCount++;
+        client.on('readyToStart', function(data){
+            var item = {id:data.id, coordinates:startCoordinates[clientCount]};
+            items.push(item);
+            clientCount++;
+            if(clientCount == 2){
+                nextStep();
+                startTimeout();
+            }
+        })
+
         client.on('nextStep', function(data){
+            clientsReady++
+            for (i = 0; i < items.length; i++) {
+                var item = items[i];
+                if (item.id == data.id) {
+                    items[i] = data;
+                }
+            }
+            if(clientsReady == clientCount){
+                nextStep();
+            }
 
         });
-        client.on('disconnect', function(){
+        client.on('disconnect', function(id){
             clientCount--;
+            for (i = 0; i < items.length; i++) {
+                var item = items[i];
+                if(item.id == id){
+                    items.slice(i,1);
+                    break;
+                }
+            }
         });
     });
 
-    io.emit('stepComplete', {x:0,y:0,rotation:90});
+    clientsReady = 0;
+
+}
+
+function nextStep(){
+    clientsReady = 0;
+    clearTimeout(timeout);
+    updateData();
+    startTimeout();
+}
+
+function startTimeout(){
+    timeout = setTimeout(nextStep,5000);
+}
+
+function updateData(){
+    io.in('game').emit('stepComplete', items);
 }
 
 module.exports = setSocket;
