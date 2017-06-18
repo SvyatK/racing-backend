c4cdbc8ab7a2c311c46f3a67ce06992b7c64220e
AndyGura <andy.gura.jd@gmail.com>
2017-06-19 00:07:11 +0300

    fixed scope hell in socket wrapper

--- services/socketWrapper.js
+++ services/socketWrapper.js
@@ -9,40 +9,89 @@ var timeout;
 
 var items = [];
 
-var startCoordinates = [{x:3104, y:2287, rot:-1.49},{x:3175, y:2292, rot:-1.49}]
-
 var setSocket = function (data) {
+    
+    this.update = function update(socket){
+        clientsReady = 0;
+        //clearTimeout(timeout);
+        this.updateData(socket);
+        //startTimeout();
+    }
+
+    this.updateData = function updateData(socket){
+        //var rooms = Objects.keys(socket.rooms);
+        //console.log(rooms); // [ <socket.id>, 'room 237' ]
+        //var room = io.sockets.manager.roomClients[socket.id];
+        //console.log(room);
+        io.emit('stepComplete', this.items);
+        //io.broadcast.emit('stepComplete', items);
+    }
+    
     io = data;
     console.log(data);
-    io.on('connection', function(client){
+    io.on('connection', function(client) {
         //client.join('room1');
-        client.on('readyToStart', function(data){
-            items = [];
-            var item = {id:data.id, x:startCoordinates[0].x, y:startCoordinates[0].y};
-            items.push(item);
-            clientCount++;
-            //if(clientCount >= 2){
-                update(client);
-            //}
+        client.on('readyToStart', function(data) {
+            if (!this.items) {
+                this.items = [];
+            }
+            if (!this.clientCount) {
+                this.clientCount = 0;
+            }
+            if (!this.startCoordinates) {
+                this.startCoordinates = [{x:3104, y:2287, rot:-1.49},{x:3175, y:2292, rot:-1.49}];
+            }
+            var item = {
+                id:data.id,
+                x:this.startCoordinates[this.clientCount].x,
+                y:this.startCoordinates[this.clientCount].y,
+                rotation:this.startCoordinates[this.clientCount].rot
+            };
+            this.items.push(item);
+            this.clientCount++;
+            if (this.clientCount >= 2){
+                this.update(client);
+            }
          /*   if(clientCount>2){
                 clientCount = 0;
                 items = [];
             }*/
-        })
+        }.bind(this));
 
-        client.on('nextStep', function(data){
-            clientsReady++
-            for (var i = 0; i < items.length; i++) {
-                var item = items[i];
+        client.on('nextStep', function(data) {
+            if (!this.clientsReady) {
+                this.clientsReady = 0;
+            }
+            this.clientsReady++;
+            for (var i = 0; i < this.items.length; i++) {
+                var item = this.items[i];
                 if (item.id == data.id) {
-                    items[i] = data;
+                    this.items[i] = data;
                 }
             }
-            //if(clientsReady == 2){
-                update(client);
-            //}
+            if (this.clientsReady == 2){
+                this.update(client);
+                this.clientsReady = 0;
+            }
 
-        });
+        }.bind(this));
+
+        client.on('finished', function(data) {
+            if (!this.clientsFinished) {
+                this.clientsFinished = 0;
+            }
+            this.clientsFinished++;
+            for (var i = 0; i < this.items.length; i++) {
+                var item = this.items[i];
+                if (item.id == data.id) {
+                    this.items[i].result = data;
+                }
+            }
+            if (this.clientsFinished == 2) {
+                io.emit('raceFinished', this.items[0].result < this.items[1].result ? this.items[0].id : this.items[1].id);
+            }
+
+        }.bind(this));
         client.on('disconnect', function(id){
             clientCount--;
             for (i = 0; i < items.length; i++) {
@@ -53,27 +102,11 @@ var setSocket = function (data) {
                 }
             }
         });
-    });
-}
-
-function update(socket){
-    clientsReady = 0;
-    //clearTimeout(timeout);
-    updateData(socket);
-    //startTimeout();
+    }.bind(this));
 }
 
 function startTimeout(){
    // timeout = setTimeout(nextStep,5000);
-}
-
-function updateData(socket){
-    //var rooms = Objects.keys(socket.rooms);
-    //console.log(rooms); // [ <socket.id>, 'room 237' ]
-    //var room = io.sockets.manager.roomClients[socket.id];
-    //console.log(room);
-    io.emit('stepComplete', items);
-    //io.broadcast.emit('stepComplete', items);
 }
 
 module.exports = setSocket;
