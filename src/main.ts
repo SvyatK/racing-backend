04e6b873faf86001c3cbb381555e99ae53e26069
AndyGura <andy.gura.jd@gmail.com>
2018-03-10 01:32:13 +0200

    added serving frontend

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
-    const app = await NestFactory.create(ApplicationModule);
+    const expressInstance: express.Express = express();
+    expressInstance.use(express.static(path.join(__dirname, '../www')));
+    const app = await NestFactory.create(ApplicationModule, expressInstance, {});
     const options = new DocumentBuilder()
         .setTitle('Racing Backend')
         .setDescription('The racing backend API description')
@@ -12,7 +16,8 @@ async function bootstrap() {
         .build();
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('/api', app, document);
-    await app.listen(+process.env.PORT || 3000);
+    // TODO fix env variables
+    await app.listen(/*+process.env.PORT || */3000);
 }
 
 bootstrap();
