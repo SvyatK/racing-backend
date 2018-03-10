04813ea3b32284ad3118d494ba9d3caa2a6c9560
AndyGura <andy.gura.jd@gmail.com>
2018-03-11 01:31:19 +0200

    added https

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { ApplicationModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as express from 'express';
 import * as path from 'path';
+import * as https from 'https';
+import * as fs from 'fs';
 
 async function bootstrap() {
+    const sslOptions = {
+        key: fs.readFileSync('/etc/letsencrypt/live/example.com/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/example.com/cert.pem'),
+        ca: fs.readFileSync('/etc/letsencrypt/live/example.com/chain.pem')
+    };
     const expressInstance: express.Express = express();
     expressInstance.use(express.static(path.join(__dirname, '../www')));
-    const app = await NestFactory.create(ApplicationModule, expressInstance, {});
+    const server = https.createServer(sslOptions, expressInstance);
+    const app = await NestFactory.create(ApplicationModule, server, {});
     const options = new DocumentBuilder()
         .setTitle('Racing Backend')
         .setDescription('The racing backend API description')
