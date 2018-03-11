dbc3e0053f044ff9c763ef0c62295267096a9f58
AndyGura <andy.gura.jd@gmail.com>
2018-03-12 00:09:10 +0200

    trying to fix secured socket connection

--- src/gaming-worker/worker-main.ts
+++ src/gaming-worker/worker-main.ts
@@ -2,21 +2,21 @@ import { WorkerModule } from './worker.module';
 import { NestApplication, NestFactory } from '@nestjs/core';
 import Environment from './environment';
 import { GamingServerMainService } from './services/gaming-server-main.service';
-import * as fs from "fs";
+import * as fs from 'fs';
 import * as express from 'express';
-import * as https from "https";
-import * as http from "http";
+import * as https from 'https';
+import * as http from 'http';
 
 async function bootstrap() {
-    const sslOptions = {
+    const httpsOptions = {
         key: fs.readFileSync('/etc/letsencrypt/live/racing-demo-dev.pp.ua/privkey.pem'),
         cert: fs.readFileSync('/etc/letsencrypt/live/racing-demo-dev.pp.ua/cert.pem'),
         ca: fs.readFileSync('/etc/letsencrypt/live/racing-demo-dev.pp.ua/chain.pem')
     };
     const expressInstance: express.Express = express();
-    const app: NestApplication = await NestFactory.create(WorkerModule, expressInstance, {});
+    const app: NestApplication = await NestFactory.create(WorkerModule, expressInstance, { httpsOptions });
     await app.init();
-    await https.createServer(sslOptions, expressInstance).listen(Environment.PORT);
+    await https.createServer(httpsOptions, expressInstance).listen(Environment.PORT);
     await http.createServer(expressInstance).listen(Environment.PORT + 1000);
     const gamingServerMainService: GamingServerMainService = app.get('GamingServerMainService');
     gamingServerMainService.workerStarted();
--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,14 @@ import * as fs from 'fs';
 import * as http from 'http';
 
 async function bootstrap() {
-    const sslOptions = {
+    const httpsOptions = {
         key: fs.readFileSync('/etc/letsencrypt/live/racing-demo-dev.pp.ua/privkey.pem'),
         cert: fs.readFileSync('/etc/letsencrypt/live/racing-demo-dev.pp.ua/cert.pem'),
         ca: fs.readFileSync('/etc/letsencrypt/live/racing-demo-dev.pp.ua/chain.pem')
     };
     const expressInstance: express.Express = express();
     expressInstance.use(express.static(path.join(__dirname, '../www')));
-    const app = await NestFactory.create(ApplicationModule, expressInstance, {});
+    const app = await NestFactory.create(ApplicationModule, expressInstance, { httpsOptions });
     const options = new DocumentBuilder()
         .setTitle('Racing Backend')
         .setDescription('The racing backend API description')
@@ -27,7 +27,7 @@ async function bootstrap() {
     app.init();
     http.createServer(expressInstance).listen(8080);
     // TODO fix env variables
-    https.createServer(sslOptions, expressInstance).listen(/*+process.env.PORT || */3000);
+    https.createServer(httpsOptions, expressInstance).listen(/*+process.env.PORT || */3000);
 }
 
 bootstrap();
