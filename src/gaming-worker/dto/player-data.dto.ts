0ee5d4020e22922564f299690f3cf928e4b09396
AndyGura <andy.gura.jd@gmail.com>
2018-12-28 17:33:05 +0200

    updated frontend build

--- src/gaming-worker/dto/player-data.dto.ts
+++ src/gaming-worker/dto/player-data.dto.ts
@@ -1,6 +1,7 @@
-import { UserDataDTO } from '../../app/dto/responses/user-data.dto';
-import { CarDataDTO } from '../../app/dto/responses/car-data.dto';
+import {UserDataDTO} from '../../app/dto/responses/user-data.dto';
+import {CarDataDTO} from '../../app/dto/responses/car-data.dto';
 import PlayerModel from '../models/player.model';
+import StepDataDto from './step-data.dto';
 
 export default class PlayerDataDTO {
 
@@ -8,7 +9,7 @@ export default class PlayerDataDTO {
     public user: UserDataDTO;
     public car: CarDataDTO;
     // TODO adjust structure here; add type
-    public data: any;
+    public data: {item: StepDataDto, ready: boolean, finished: boolean};
 
     public static fromPlayerModel(playerModel: PlayerModel): PlayerDataDTO {
         return {
